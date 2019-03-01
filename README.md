b233404bc121b5f9d6f92b61299029a4ffa6f097
Lyzw57 <askuczynski57@gmail.com>
2019-03-01 21:33:34 +0100

    created README and add_one.py with solved problem

---
+++ README.md
@@ -0,0 +1,3 @@
+# Challenge #375 Print a new number by adding one to each of its digit
+
+My soultion in python 3.7.1.
---
+++ add_one.py
@@ -0,0 +1,16 @@
+def add_one_to_each(number):
+    result = 0
+    factor = 0
+
+    while number:
+        result += ((number % 10) + 1) * 10 ** factor
+        if (number % 10) + 1 < 10:
+            factor += 1
+        else:
+            factor += 2
+
+        number //= 10
+
+    return result
+
+print(add_one_to_each(8998))
